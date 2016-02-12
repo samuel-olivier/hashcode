93a4909c299c449a95f173042f357cc2aa0432bf
Hellfar <pierre.louchet@gmail.com>
2016-02-12 06:31:56 +0100

    example tests

--- pierro/Algo.rb
+++ pierro/Algo.rb
@@ -53,6 +53,35 @@ end
 
 if __FILE__ == $0
 
-	Algo.new
+	require_relative 'Map'
+	require_relative 'ProductType'
+	require_relative 'Warehouse'
+	require_relative 'Order'
+	require_relative 'Drone'
+
+	# below the subject example
+	map = Map.new 100, 100
+	map.add :productType, ProductType.new(0, 100)
+	map.add :productType, ProductType.new(0, 5)
+	map.add :productType, ProductType.new(0, 450)
+	warehouse = Warehouse.new(0, 0)
+	warehouse.add 5
+	warehouse.add 1
+	warehouse.add 0
+	map.add :drone, Drone.new(warehouse, 500)
+	map.add :drone, Drone.new(warehouse, 500)
+	map.add :drone, Drone.new(warehouse, 500)
+	map.add :warehouse, warehouse
+	warehouse = Warehouse.new(5, 5)
+	warehouse.add 0
+	warehouse.add 10
+	warehouse.add 2
+	map.add :warehouse, warehouse
+	map.add :order, Order.new(1, 1, [map.productTypes[2], map.productTypes[0]])
+	map.add :order, Order.new(3, 3, [map.productTypes[0], map.productTypes[0], map.productTypes[0]])
+	map.add :order, Order.new(5, 6, [map.productTypes[2]])
+
+p map
+	# Algo.new map
 
 end
