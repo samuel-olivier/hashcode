8613de2c309a36efec38c318a0e9e0a80a8b12d2
Hellfar <pierre.louchet@gmail.com>
2016-02-12 02:59:19 +0100

    No file fix

--- pierro/main.rb
+++ pierro/main.rb
@@ -19,44 +19,46 @@ if __FILE__ == $0
 	end.parse!
 
 	lines = []
-	File.open(options[:inputfile]).each do |line|
+	(options[:inputfile] ? File.open(options[:inputfile]) : $<).each do |line|
 		lines << line.split
-	end if options[:inputfile]
-
-	# map
-	row, column, nbDrones, turns, maxPayload = lines.shift
-	map = Map.new row, column
-
-	# productTypes
-	nbProductType = lines.shift[0]
-	productTypes = []
-	lines.shift.each do |weigh|
-		productTypes << ProductType.new(weigh)
 	end
 
-	# warehouses
-	nbWarehouse = lines.shift.first.to_i
-	while nbWarehouse > 0 do
-		row, column = lines.shift
-		map.add :warehouse, Warehouse.new(row, column)
-		lines.shift.each do |nbProduct|
-			map.warehouses(:last).add nbProduct
+	if lines.count > 4
+		# map
+		row, column, nbDrones, turns, maxPayload = lines.shift
+		map = Map.new row, column
+
+		# productTypes
+		nbProductType = lines.shift.first.to_i
+		productTypes = []
+		lines.shift.each do |weigh|
+			productTypes << ProductType.new(weigh)
 		end
-		nbWarehouse -= 1
-	end
 
-	# orders
-	nbOrder = lines.shift.first.to_i
-	orders = []
-	while nbOrder > 0 do
-		row, column = lines.shift
-		nbItem = lines.shift.first.to_i
-		orders << Order.new(row, column, lines.shift)
-		nbOrder -= 1
+		# warehouses
+		nbWarehouse = lines.shift.first.to_i
+		while nbWarehouse > 0 do
+			row, column = lines.shift
+			map.add :warehouse, Warehouse.new(row, column)
+			lines.shift.each do |nbProduct|
+				map.warehouses(:last).add nbProduct
+			end
+			nbWarehouse -= 1
+		end
+
+		# orders
+		nbOrder = lines.shift.first.to_i
+		orders = []
+		while nbOrder > 0 do
+			row, column = lines.shift
+			nbItem = lines.shift.first.to_i
+			orders << Order.new(row, column, lines.shift)
+			nbOrder -= 1
+		end
+		# p map.warehouses :first
+		# p orders
+		# p lines
+		# drones = Drone.new
 	end
-	# p map.warehouses :first
-	p orders
-	# p lines
-	# drones = Drone.new
 
 end
